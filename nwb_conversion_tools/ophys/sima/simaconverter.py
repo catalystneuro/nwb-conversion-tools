da7a4aa13c983f62570bece9e5d47ab805b936c1
Saksham Sharda <saksham20.sharda@gmail.com>
2020-05-08 22:06:40 -0400

    script run test pass

--- nwb_conversion_tools/ophys/sima/simaconverter.py
+++ nwb_conversion_tools/ophys/sima/simaconverter.py
@@ -1,8 +1,10 @@
 from nwb_conversion_tools.ophys import ProcessedOphysNWBConverter
 from segmentationextractors import SimaSegmentationExtractor
-from segmentationextractors.nwbsegmentationextractor import iter_datasetvieww
-from pynwb.ophys import Fluorescence
+from segmentationextractors.nwbextractor.nwbsegmentationextractor import iter_datasetvieww
+from pynwb.ophys import Fluorescence, TwoPhotonSeries
 from hdmf.data_utils import DataChunkIterator
+import yaml
+from datetime import datetime
 
 class Sima2NWB(ProcessedOphysNWBConverter):
 
@@ -10,25 +12,54 @@ class Sima2NWB(ProcessedOphysNWBConverter):
         #TODO: add channel names from sima instead of default. Or should that be input from gui only? In all other extractors, its none anyway
         if isinstance(source_path,str):
             self.source_paths={'sima_folder': dict(type='folder', path=source_path)}
-
-        self.sima_obj = SimaSegmentationExtractor(source_path['sima_folder']['path'])
+        else:
+            self.source_paths=source_path
+        if isinstance(metadata,str):
+            with open(metadata,'r') as f:
+                metadata = yaml.load(f, Loader=yaml.FullLoader)
+        self.sima_obj = SimaSegmentationExtractor(self.source_paths['sima_folder']['path'])
         super().__init__(metadata,nwbfile,source_path)
-        self.create_plane_segmentation()
+        self.auto_create_dict = dict(imaging_plane=False,
+                                     two_photon_series=True,
+                                     plane_segmentation=True)
 
+    def create_two_photon_series(self, metadata=None, imaging_plane=None):
+        if imaging_plane is None:
+            if self.nwbfile.imaging_planes:
+                imaging_plane = self.nwbfile.imaging_planes[list(self.nwbfile.imaging_planes.keys())[0]]
+            else:
+                imaging_plane = self.add_imaging_plane()
 
-    def add_optical_channel(self):
+        input_kwargs = dict(
+            name='TwoPhotonSeries',
+            description='no description',
+            external_file=[self.sima_obj.get_movie_location()],
+            format='external',
+            rate=self.sima_obj.get_sampling_frequency(),
+            starting_time=0.0,
+            imaging_plane=imaging_plane
+        )
+
+        if metadata is None and 'Ophys' in self.metadata and 'TwoPhotonSeries' in self.metadata['Ophys']:
+            metadata = self.metadata['Ophys']['TwoPhotonSeries']
+        if metadata:
+            input_kwargs.update(metadata)
+
+        return self.nwbfile.add_acquisition(TwoPhotonSeries(**input_kwargs))
+
+    def create_imaging_plane(self, optical_channel_list=None):
         """
-        Metadata is a list of optical channels. Each value in the format of the metafile.schema
-        :param metadata:
+        :param optical_channel_list:
         :return:
         """
-        optical_channel_list = []
+        if not optical_channel_list:
+            optical_channel_list = []
         channel_names = self.sima_obj.get_channel_names()
-        template_dict = dict(Ophys=dict(OpticalChannel=dict(name='name')))
+        template_dict = dict(name='name')
         for i in channel_names:
-            channel_dict = template_dict['Ophys']['OpticalChannel']['name'] = i
-            optical_channel_list.append(self.create_optical_channel(channel_dict))
-        self.add_imaging_plane(optical_channel=optical_channel_list)
+            template_dict['name'] = i
+            optical_channel_list.append(self.create_optical_channel(template_dict))
+        self.imaging_plane = self.add_imaging_plane(optical_channel=optical_channel_list)
 
     def add_rois(self):
         for i, roiid in enumerate(self.sima_obj.roi_idx):
@@ -39,7 +70,7 @@ class Sima2NWB(ProcessedOphysNWBConverter):
         input_kwargs = dict(
             name='RoiResponseSeries',
             description='no description',
-            rois=self.create_roi_table_region(self.sima_obj.image_masks.shape[-1]),
+            rois=self.create_roi_table_region(list(range(self.sima_obj.image_masks.shape[-1]))),
             starting_time=0.0,
             rate=self.sima_obj.get_sampling_frequency(),
             unit='lumens'
@@ -53,7 +84,8 @@ class Sima2NWB(ProcessedOphysNWBConverter):
         for i in metadata_iter:
             input_kwargs.update(**i)
             input_kwargs.update(
-                data=DataChunkIterator(data=iter_datasetvieww(self.sima_obj.roi_response))
+                # data=DataChunkIterator(data=iter_datasetvieww(self.sima_obj.roi_response))
+                data = self.sima_obj.roi_response
             )
             fl.create_roi_response_series(**input_kwargs)
 
@@ -65,4 +97,8 @@ class Sima2NWB(ProcessedOphysNWBConverter):
         To populate the nwb file completely.
         :return:
         """
-        pass
+        self.create_imaging_plane()
+        self.create_plane_segmentation()
+        self.add_rois()
+        self.add_fluorescence_traces()
+        self.create_two_photon_series(imaging_plane=self.nwbfile.get_imaging_plane(name='ImagingPlane'))
