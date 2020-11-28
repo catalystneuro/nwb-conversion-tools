211812c963e78f6ecc0125b44ee1bb8120f78a4e
Cody Baker <cbaker9@nd.edu>
2020-11-28 16:14:55 -0500

    reverted dimension check in BaseSorting run_conversion

--- nwb_conversion_tools/basesortingextractorinterface.py
+++ nwb_conversion_tools/basesortingextractorinterface.py
@@ -55,14 +55,13 @@ class BaseSortingExtractorInterface(BaseDataInterface, ABC):
             sorting_extractor = self.sorting_extractor
 
         for metadata_column in metadata['UnitProperties']:
-            if len(metadata_column['data']) == len(sorting_extractor.get_unit_ids()):
-                property_descriptions.update({metadata_column['name']: metadata_column['description']})
-                for unit_id in sorting_extractor.get_unit_ids():
-                    if metadata_column['name'] == 'electrode_group':
-                        data = nwbfile.electrode_groups[metadata_column['data'][unit_id]]
-                    else:
-                        data = metadata_column['data'][unit_id]
-                    sorting_extractor.set_unit_property(unit_id, metadata_column['name'], data)
+            property_descriptions.update({metadata_column['name']: metadata_column['description']})
+            for unit_id in sorting_extractor.get_unit_ids():
+                if metadata_column['name'] == 'electrode_group':
+                    data = nwbfile.electrode_groups[metadata_column['data'][unit_id]]
+                else:
+                    data = metadata_column['data'][unit_id]
+                sorting_extractor.set_unit_property(unit_id, metadata_column['name'], data)
 
         se.NwbSortingExtractor.write_sorting(
             sorting_extractor,
