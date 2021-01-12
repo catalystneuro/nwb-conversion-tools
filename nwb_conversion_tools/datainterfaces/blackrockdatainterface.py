5ba51615aea64e3489d5f744ed65a2773a286dbc
luiz <luiz_tauffer@hotmail.com>
2021-01-12 19:20:56 +0100

    create data interface

---
+++ nwb_conversion_tools/datainterfaces/blackrockdatainterface.py
@@ -0,0 +1,19 @@
+"""Authors: Luiz Tauffer"""
+import spikeextractors as se
+
+from ..baserecordingextractorinterface import BaseRecordingExtractorInterface
+from ..json_schema_utils import get_schema_from_method_signature
+
+
+class BlackrockRecordingInterface(BaseRecordingExtractorInterface):
+    """Primary data interface class for converting a BlackrockRecordingExtractor."""
+
+    RX = se.BlackrockRecordingExtractor
+
+    @classmethod
+    def get_source_schema(cls):
+        """Compile input schema for the RecordingExtractor."""
+        return get_schema_from_method_signature(
+            class_method=cls.RX.__init__,
+            exclude=['block_index', 'seg_index_index']
+        )
