fa9c738b43ffec62ea11ac9f2d85c079f6525977
luiz <luiz_tauffer@hotmail.com>
2021-03-03 14:18:24 +0100

    small fix on blackrock, add neo to requirements

--- nwb_conversion_tools/datainterfaces/blackrockdatainterface.py
+++ nwb_conversion_tools/datainterfaces/blackrockdatainterface.py
@@ -28,6 +28,8 @@ class BlackrockRecordingExtractorInterface(BaseRecordingExtractorInterface):
             exclude=['block_index', 'seg_index']
         )
         metadata_schema['additionalProperties'] = True
+        metadata_schema['properties']['filename']['format'] = 'file'
+        metadata_schema['properties']['filename']['description'] = 'Path to Blackrock file.'
         return metadata_schema
     
     def __init__(self, filename: PathType, nsx_to_load: Optional[int] = None):
@@ -110,6 +112,18 @@ class BlackrockSortingExtractorInterface(BaseSortingExtractorInterface):
     """Primary data interface class for converting Blackrock spiking data."""
 
     SX = se.BlackrockSortingExtractor
+
+    @classmethod
+    def get_source_schema(cls):
+        """Compile input schema for the RecordingExtractor."""
+        metadata_schema = get_schema_from_method_signature(
+            class_method=cls.SX.__init__,
+            exclude=['block_index', 'seg_index', 'nsx_to_load']
+        )
+        metadata_schema['additionalProperties'] = True
+        metadata_schema['properties']['filename']['format'] = 'file'
+        metadata_schema['properties']['filename']['description'] = 'Path to Blackrock file.'
+        return metadata_schema
 
     def __init__(self, filename: PathType, nsx_to_load: Optional[int] = None):
         super().__init__(filename=filename, nsx_to_load=nsx_to_load)
