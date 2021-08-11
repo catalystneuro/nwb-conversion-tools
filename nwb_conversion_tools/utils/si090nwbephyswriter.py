7ada89dd3ae4e6d1ac8e69e5e4e834624726f341
Saksham Sharda <saksham20.sharda@gmail.com>
2021-08-11 17:57:42 +0530

    moving electrode groups to base class

--- nwb_conversion_tools/utils/si090nwbephyswriter.py
+++ nwb_conversion_tools/utils/si090nwbephyswriter.py
@@ -97,9 +97,6 @@ class SI090NwbEphysWriter(BaseNwbEphysWriter):
     def add_electrodes(self):
         raise NotImplementedError
 
-    def add_electrode_groups(self):
-        raise NotImplementedError
-
     def add_electrical_series(self):
         raise NotImplementedError
 
