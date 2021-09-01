0983b0e482c47b92f7be06104759d3a8f4a1fb20
luiz <luiz_tauffer@hotmail.com>
2021-09-01 16:19:54 +0200

    abf session start time

--- nwb_conversion_tools/datainterfaces/icephys/abf/abf_neo_datainterface.py
+++ nwb_conversion_tools/datainterfaces/icephys/abf/abf_neo_datainterface.py
@@ -2,6 +2,9 @@ from ..base_interface_icephys_neo import BaseIcephysNeoInterface
 from ....utils.json_schema import get_schema_from_method_signature
 from neo import AxonIO
 
+from datetime import datetime, timedelta
+import pytz
+
 
 class AbfNeoDataInterface(BaseIcephysNeoInterface):
     """ABF DataInterface based on Neo AxonIO"""
@@ -21,3 +24,28 @@ class AbfNeoDataInterface(BaseIcephysNeoInterface):
 
     def __init__(self, file_path: str):
         super().__init__(filename=file_path)
+
+    def get_metadata(self):
+        """Auto-fill as much of the metadata as possible. Must comply with metadata schema."""
+        metadata = super().get_metadata()
+
+        # Extract start_time info
+        startDate = str(self.reader._axon_info['uFileStartDate'])
+        startTime = round(self.reader._axon_info['uFileStartTimeMS']/1000)
+        startDate = datetime.strptime(startDate, "%Y%M%d")
+        startTime = timedelta(seconds=startTime)
+        abfDateTime = startDate+startTime
+        # Time Zone
+        session_start_time_tzaware = pytz.timezone("UTC").localize(abfDateTime)
+        session_start_time_tzaware = session_start_time_tzaware.strftime("%Y-%m-%dT%H:%M:%S%z")
+        # Add a colon separator to the TZ offset segment 
+        session_start_time_tzaware = "{0}:{1}".format(
+            session_start_time_tzaware[:-2],
+            session_start_time_tzaware[-2:]
+        )
+
+        metadata["NWBFile"] = dict(
+            session_start_time=session_start_time_tzaware,
+        )
+
+        return metadata
