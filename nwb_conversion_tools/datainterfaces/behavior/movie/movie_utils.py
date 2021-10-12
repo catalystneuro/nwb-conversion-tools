6bbc6195f4b1b1fba1bfc5c71b1c407b4052b633
bendichter <ben.dichter@gmail.com>
2021-10-12 16:22:40 -0400

    style improvements for VideoCaptureContext

--- nwb_conversion_tools/datainterfaces/behavior/movie/movie_utils.py
+++ nwb_conversion_tools/datainterfaces/behavior/movie/movie_utils.py
@@ -17,50 +17,44 @@ PathType = Union[str, Path]
 
 class VideoCaptureContext(cv2.VideoCapture):
     def __init__(self, *args, stub=False, **kwargs):
-        self._args = args
-        self._kwargs = kwargs
-        self._stub = stub
-        self._current_frame = 0
-        with self:
-            self._frame_count = self._get_movie_frame_count()
-            self._fps = self._get_movie_fps()
-            self._frame = self.get_movie_frame(0)
-            assert self._frame is not None, "unable to read the movie file provided"
+        super().__init__(*args, **kwargs)
+        self.stub = stub
+        self.current_frame = 0
+        self.frame_count = self.get_movie_frame_count()
+        self.fps = self.get_movie_fps()
+        self.frame = self.get_movie_frame(0)
+        assert self.frame is not None, "unable to read the movie file provided"
 
     def get_movie_timestamps(self):
         """
         Return numpy array of the timestamps for a movie file.
 
         """
-        if self.isOpened():
-            ts = [self.get(cv2.CAP_PROP_POS_MSEC)]
-            for i in tqdm(range(1, self.get_movie_frame_count()), desc="retrieving video timestamps"):
-                self._set_frame(i)
-                ts.append(self.get(cv2.CAP_PROP_POS_MSEC))
-            self._set_frame(0)
-            return np.array(ts)
+        if not self.isOpened():
+            raise ValueError('movie file is not open')
+        ts = [self.get(cv2.CAP_PROP_POS_MSEC)]
+        for i in tqdm(range(1, self.get_movie_frame_count()), desc="retrieving video timestamps"):
+            self._set_frame(i)
+            ts.append(self.get(cv2.CAP_PROP_POS_MSEC))
+        self._set_frame(0)
+        return np.array(ts)
 
-    def _get_movie_fps(self):
+    def get_movie_fps(self):
         """
         Return the internal frames per second (fps) for a movie file.
 
         """
-        if int((cv2.__version__).split(".")[0]) < 3:
-            fps = self.get(cv2.cv.CV_CAP_PROP_FPS)
-        else:
-            fps = self.get(cv2.CAP_PROP_FPS)
-        return fps
-
-    def get_movie_fps(self):
-        return self._fps
+        if int(cv2.__version__.split(".")[0]) < 3:
+            return self.get(cv2.cv.CV_CAP_PROP_FPS)
+        return self.get(cv2.CAP_PROP_FPS)
 
     def get_frame_shape(self) -> Tuple:
         """
         Return the shape of frames from a movie file.
         """
-        return self._frame.shape
+        return self.frame.shape
 
-    def _get_movie_frame_count(self):
+    def get_movie_frame_count(self):
         """
         Return the total number of frames for a movie file.
 
@@ -68,66 +62,59 @@ class VideoCaptureContext(cv2.VideoCapture):
         if self._stub:
             # if stub the assume a max frame count of 10
             return 10
-        if int((cv2.__version__).split(".")[0]) < 3:
-            count = self.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT)
+        if int(cv2.__version__.split(".")[0]) < 3:
+            return self.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT)
         else:
             count = self.get(cv2.CAP_PROP_FRAME_COUNT)
         return int(count)
 
-    def get_movie_frame_count(self):
-        return self._frame_count
-
-    def _set_frame(self, frame_no):
-        if int((cv2.__version__).split(".")[0]) < 3:
+    def set_frame(self, frame_no):
+        if int(cv2.__version__.split(".")[0]) < 3:
             set_arg = cv2.cv.CV_CAP_PROP_POS_FRAMES
         else:
             set_arg = cv2.CAP_PROP_POS_FRAMES
-        self._current_frame = frame_no
+        self.current_frame = frame_no
         return self.set(set_arg, frame_no)
 
     def get_movie_frame(self, frame_no: int):
         """
         Return the specific frame from a movie.
         """
-        if self.isOpened():
-            assert frame_no < self.get_movie_frame_count()
-            _ = self._set_frame(frame_no)
-            success, frame = self.read()
-            _ = self._set_frame(0)
-            if success:
-                return frame
-            elif frame_no > 0:
-                return np.nan * np.ones(self.get_frame_shape())
+        if not self.isOpened():
+            raise ValueError("movie file is not open")
+        assert frame_no < self.get_movie_frame_count(), "frame number is greater than length of movie"
+        _ = self._set_frame(frame_no)
+        success, frame = self.read()
+        _ = self._set_frame(0)
+        if success:
+            return frame
+        elif frame_no > 0:
+            return np.nan * np.ones(self.get_frame_shape())
 
     def get_movie_frame_dtype(self):
         """
         Return the dtype for frame in a movie file.
         """
-        return self._frame.dtype
+        return self.frame.dtype
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        if self.isOpened():
-            try:
-                if self._current_frame < self.get_movie_frame_count():
-                    success, frame = self.read()
-                    self._current_frame += 1
-                    if success:
-                        return frame
-                    else:
-                        return np.nan * np.ones(self.get_frame_shape())
-                else:
-                    _ = self._set_frame(0)
-                    raise StopIteration
-            except Exception:
+        if not self.isOpened():
+            raise StopIteration
+        try:
+            if self.current_frame < self.get_movie_frame_count():
+                yield self.get_movie_frame(self._current_frame)
+                self.current_frame += 1
+            else:
+                _ = self._set_frame(0)
                 raise StopIteration
-        else:
+        except Exception:
             raise StopIteration
 
-    def __enter__(self):
-        super(VideoCaptureContext, self).__init__(*self._args, **self._kwargs)
+    def __enter__(self, *args, **kwargs):
+        self.__init__(*args, **kwargs)
         return self
 
     def __exit__(self, *args):
