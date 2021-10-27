d10da66a81fd8b6141ab9d33e032d326309a6d78
Heberto Mayorquin <h.mayorquin@gmail.com>
2021-10-27 08:45:13 +0200

    Uncommented some lines and black compliance

--- nwb_conversion_tools/datainterfaces/ecephys/neuroscope/neuroscopedatainterface.py
+++ nwb_conversion_tools/datainterfaces/ecephys/neuroscope/neuroscopedatainterface.py
@@ -243,8 +243,8 @@ class NeuroscopeSortingInterface(BaseSortingExtractorInterface):
         session_path = Path(self.source_data["folder_path"])
         session_id = session_path.stem
         metadata = {}
-        # metadata = NeuroscopeRecordingInterface.get_ecephys_metadata(
-        #    xml_file_path=str((session_path / f"{session_id}.xml").absolute())
-        # )
-        # metadata.update(UnitProperties=[])
+        metadata = NeuroscopeRecordingInterface.get_ecephys_metadata(
+           xml_file_path=str((session_path / f"{session_id}.xml").absolute())
+        )
+        metadata.update(UnitProperties=[])
         return metadata
