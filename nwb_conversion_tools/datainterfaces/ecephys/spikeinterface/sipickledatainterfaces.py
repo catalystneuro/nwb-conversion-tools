4be9466b91ce114ad919fa8f03b2ec21ef563edb
Saksham Sharda <saksham20.sharda@gmail.com>
2021-11-05 16:52:57 +0530

    fix tests

--- nwb_conversion_tools/datainterfaces/ecephys/spikeinterface/sipickledatainterfaces.py
+++ nwb_conversion_tools/datainterfaces/ecephys/spikeinterface/sipickledatainterfaces.py
@@ -10,20 +10,16 @@ from ....utils import map_si_object_to_writer
 class SIPickleRecordingExtractorInterface(BaseRecordingExtractorInterface):
     """Primary interface for reading and converting SpikeInterface Recording objects through .pkl files."""
 
-    RX = None
+    RX = load_extractor_from_pickle
 
     def __init__(self, pkl_file: FilePathType):
-        self.recording_extractor = load_extractor_from_pickle(pkl_file=pkl_file)
-        self.subset_channels = None
-        self.source_data = dict(pkl_file=pkl_file)
+        super(SIPickleRecordingExtractorInterface, self).__init__(pkl_file=pkl_file)
 
 
 class SIPickleSortingExtractorInterface(BaseSortingExtractorInterface):
     """Primary interface for reading and converting SpikeInterface Sorting objects through .pkl files."""
 
-    SX = None
+    SX = load_extractor_from_pickle
 
     def __init__(self, pkl_file: FilePathType):
-        self.sorting_extractor = load_extractor_from_pickle(pkl_file=pkl_file)
-        self.source_data = dict(pkl_file=pkl_file)
-        self.writer_class = map_si_object_to_writer(self.sorting_extractor)(self.sorting_extractor)
+        super(SIPickleSortingExtractorInterface, self).__init__(pkl_file=pkl_file)
