e9d79f6dd452e568da5aaae6f7e58c194ada43e0
Cody Baker <51133164+CodyCBakerPhD@users.noreply.github.com>
2021-11-06 13:57:22 -0400

    fix test path

--- tests/test_gin.py
+++ tests/test_gin.py
@@ -28,7 +28,7 @@ except ImportError:
 #   ecephys: https://gin.g-node.org/NeuralEnsemble/ephy_testing_data
 #   ophys: TODO
 #   icephys: TODO
-LOCAL_PATH = Path("E:/GIN/")  # Must be set to "." for CI - temporarily override for local testing
+LOCAL_PATH = Path(".)  # Must be set to "." for CI - temporarily override for local testing
 DATA_PATH = LOCAL_PATH / "ephy_testing_data"
 HAVE_DATA = DATA_PATH.exists()
 
