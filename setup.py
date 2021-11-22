d016b23b3010af0996cc8a604be26838e66bf1f3
bendichter <ben.dichter@gmail.com>
2021-11-22 15:21:29 -0800

    relax pip dependencies

--- setup.py
+++ setup.py
@@ -12,23 +12,13 @@ here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, "README.md")) as f:
     long_description = f.read()
 
-# Get requirements
-if os.environ.get("NWB_CONVERSION_INSTALL_MODE", None) == "development":
-    req_file = "requirements-dev.txt"
-    print("installing nwb_conversion_tools on development mode")
-else:
-    req_file = "requirements.txt"
-
-with open(os.path.join(here, req_file)) as f:
-    install_requires = f.read().strip().split("\n")
-
 # Get remote version
 remote_version = subprocess.run(["git", "describe", "--tags"], stdout=subprocess.PIPE).stdout.decode("utf-8").strip()
 assert "." in remote_version
 
 setup(
     name="nwb-conversion-tools",
-    version="0.9.4",
+    version="0.9.5",
     description="Convert data from proprietary formats to NWB format.",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -40,7 +30,24 @@ setup(
     package_data={"": ["template_metafile.yml"]},
     include_package_data=True,
     python_requires=">=3.7",
-    install_requires=install_requires,
+    install_requires=[
+        "pynwb",
+        "tqdm",
+        "natsort",
+        "numpy",
+        "pandas",
+        "scipy",
+        "h5py",
+        "PyYAML",
+        "jsonschema",
+        "psutil",
+        "lxml",
+        "spikeextractors",
+        "spikesorters",
+        "spiketoolkit",
+        "neo",
+        "roiextractors",
+    ],
     entry_points={
         "console_scripts": ["nwb-gui=nwb_conversion_tools.gui.command_line:main"],
     },
