1be9822290471da03412b5a9b165efc4084e4be6
luiz <luiz_tauffer@hotmail.com>
2022-01-06 11:38:47 +0100

    clean converter

--- nwb_conversion_tools/datainterfaces/icephys/abf/guagwei_converter.py
+++ nwb_conversion_tools/datainterfaces/icephys/abf/guagwei_converter.py
@@ -1,11 +1,4 @@
-from pynwb import NWBFile
 from nwb_conversion_tools import AbfNeoDataInterface, NWBConverter
-from nwb_conversion_tools.utils.conversion_tools import make_nwbfile_from_metadata
-from ndx_dandi_icephys import DandiIcephysMetadata
-from typing import Optional
-import json
-from pathlib import Path
-import pandas as pd
 
 
 class GuagweiConverter(NWBConverter):
