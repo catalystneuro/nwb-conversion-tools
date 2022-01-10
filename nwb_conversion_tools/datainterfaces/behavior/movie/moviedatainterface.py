31c5d3dc8264855522116c1c1d3e9eb5b8cf0015
Saksham Sharda <saksham20.sharda@gmail.com>
2022-01-10 12:23:08 +0530

    multiple movie files for same imageSeries if extenral_mode is True

--- nwb_conversion_tools/datainterfaces/behavior/movie/moviedatainterface.py
+++ nwb_conversion_tools/datainterfaces/behavior/movie/moviedatainterface.py
@@ -16,7 +16,6 @@ from ....utils.conversion_tools import check_regular_timestamps, get_module
 from ....utils.json_schema import get_schema_from_hdmf_class, get_base_schema
 from .movie_utils import MovieDataChunkIterator, VideoCaptureContext
 
-
 try:
     import cv2
 
@@ -74,21 +73,21 @@ class MovieInterface(BaseDataInterface):
         return metadata
 
     def run_conversion(
-        self,
-        nwbfile: NWBFile,
-        metadata: dict,
-        stub_test: bool = False,
-        external_mode: bool = True,
-        starting_times: Optional[list] = None,
-        chunk_data: bool = True,
-        module_name: Optional[str] = None,
-        module_description: Optional[str] = None,
-        buffer_gb: float = None,
-        buffer_shape: list = None,
-        chunk_mb: float = None,
-        chunk_shape: list = None,
-        iterator_type: str = "v2",
-        compression: str = "gzip",
+            self,
+            nwbfile: NWBFile,
+            metadata: dict,
+            stub_test: bool = False,
+            external_mode: bool = True,
+            starting_times: Optional[list] = None,
+            chunk_data: bool = True,
+            module_name: Optional[str] = None,
+            module_description: Optional[str] = None,
+            buffer_gb: float = None,
+            buffer_shape: list = None,
+            chunk_mb: float = None,
+            chunk_shape: list = None,
+            iterator_type: str = "v2",
+            compression: str = "gzip",
     ):
         """
         Convert the movie data files to ImageSeries and write them in the NWBFile.
@@ -111,6 +110,8 @@ class MovieInterface(BaseDataInterface):
             and may contain most keywords normally accepted by an ImageSeries
             (https://pynwb.readthedocs.io/en/stable/pynwb.image.html#pynwb.image.ImageSeries).
              The list for the 'Movies' key should correspond one to one to the movie files in the file_paths list.
+             If multiple movies need to be in the same ImageSeries, then supply the same value for "name" key.
+             Multiple movies in same ImageSeries is only supported if 'external_mode'=True.
         stub_test : bool, optional
             If True, truncates the write operation for fast testing. The default is False.
         external_mode : bool, optional
@@ -153,9 +154,9 @@ class MovieInterface(BaseDataInterface):
         assert iterator_type in ["v1", "v2"], "for new iterator using GenericDatachunkIteartor use v2, else v1"
         if starting_times is not None:
             assert (
-                isinstance(starting_times, list)
-                and all([isinstance(x, float) for x in starting_times])
-                and len(starting_times) == len(file_paths)
+                    isinstance(starting_times, list)
+                    and all([isinstance(x, float) for x in starting_times])
+                    and len(starting_times) == len(file_paths)
             ), "Argument 'starting_times' must be a list of floats in one-to-one correspondence with 'file_paths'!"
         else:
             starting_times = [0.0]
@@ -168,12 +169,31 @@ class MovieInterface(BaseDataInterface):
             f"({len(image_series_kwargs_list)}) vs. file_paths ({len(self.source_data['file_paths'])})!"
         )
 
-        for j, file in enumerate(file_paths):
-            image_series_kwargs = dict(image_series_kwargs_list[j])
+        # check for duplicates in image_series_kwargs list keys:
+        def _check_duplicates(image_series_kwargs_list):
+            image_series_kwargs_list_keys = [i["name"] for i in image_series_kwargs_list]
+            if len(set(image_series_kwargs_list_keys)) < len(image_series_kwargs_list_keys):
+                assert external_mode, 'for multiple video files under the same ImageSeries name, use exernal_mode=True'
+            keys_set = []
+            image_series_kwargs_list_unique = []
+            for no, image_series_kwargs in enumerate(image_series_kwargs_list):
+                if image_series_kwargs["name"] not in keys_set:
+                    keys_set.append(image_series_kwargs["name"])
+                    image_series_kwargs_list_unique.append(dict(image_series_kwargs, data=[file_paths[no]]))
+                else:
+                    idx = keys_set.index(image_series_kwargs["name"])
+                    image_series_kwargs_list_unique[idx]["data"].append(file_paths[no])
+            return image_series_kwargs_list_unique
+
+        image_series_kwargs_list_updated = _check_duplicates(image_series_kwargs_list)
+
+        for j, image_series_kwargs in enumerate(image_series_kwargs_list_updated):
+            file_list = image_series_kwargs.pop("data")
             if external_mode:
-                image_series_kwargs.update(format="external", external_file=[file])
+                image_series_kwargs.update(format="external", external_file=file_list)
             else:
-                uncompressed_estimate = Path(file).stat().st_size * 70
+                file = file_list[0]
+                uncompressed_estimate = Path(file).stat().st_size*70
                 available_memory = psutil.virtual_memory().available
                 if not chunk_data and not stub_test and uncompressed_estimate >= available_memory:
                     warn(
@@ -213,10 +233,10 @@ class MovieInterface(BaseDataInterface):
                     else:
                         iterable = []
                         with tqdm(
-                            desc=f"Reading movie data for {Path(file).name}",
-                            position=tqdm_pos,
-                            total=total_frames,
-                            mininterval=tqdm_mininterval,
+                                desc=f"Reading movie data for {Path(file).name}",
+                                position=tqdm_pos,
+                                total=total_frames,
+                                mininterval=tqdm_mininterval,
                         ) as pbar:
                             for frame in video_capture_ob:
                                 iterable.append(frame)
@@ -236,7 +256,7 @@ class MovieInterface(BaseDataInterface):
                     fps = vc.get_movie_fps()
                     image_series_kwargs.update(starting_time=starting_times[j], rate=fps)
                 else:
-                    image_series_kwargs.update(timestamps=H5DataIO(timestamps, compression="gzip"))
+                    image_series_kwargs.update(timestamps=timestamps)
 
             if module_name is None:
                 nwbfile.add_acquisition(ImageSeries(**image_series_kwargs))
