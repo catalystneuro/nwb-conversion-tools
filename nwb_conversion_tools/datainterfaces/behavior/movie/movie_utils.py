c69130aeec7ea622e87e510788dc2cf9fa638cca
Saksham Sharda <saksham20.sharda@gmail.com>
2022-02-28 23:34:59 +0530

    typo

--- nwb_conversion_tools/datainterfaces/behavior/movie/movie_utils.py
+++ nwb_conversion_tools/datainterfaces/behavior/movie/movie_utils.py
@@ -164,7 +164,7 @@ class MovieDataChunkIterator(GenericDataChunkIterator):
     def _get_default_chunk_shape(self, chunk_mb):
         """Shape is either one frame or a subset: scaled frame size but with all pixel colors"""
         num_frames = int(chunk_mb // self._full_frame_size_mb)
-        num_frames = 1 if no_frames is 0 else no_frames
+        num_frames = 1 if num_frames is 0 else num_frames
         return (num_frames,) + self._full_frame_shape[1:]
 
     def _get_default_buffer_shape(self, buffer_gb):
