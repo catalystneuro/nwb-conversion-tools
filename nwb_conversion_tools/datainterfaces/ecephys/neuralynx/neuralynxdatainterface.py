ba5a1c19450abc6ba78e09be8d48669a3522e5c9
bendichter <ben.dichter@gmail.com>
2022-03-24 16:16:54 -0600

    get filtering

--- nwb_conversion_tools/datainterfaces/ecephys/neuralynx/neuralynxdatainterface.py
+++ nwb_conversion_tools/datainterfaces/ecephys/neuralynx/neuralynxdatainterface.py
@@ -1,4 +1,5 @@
 """Authors: Cody Baker and Ben Dichter."""
+import warnings
 from pathlib import Path
 from natsort import natsorted
 from glob import glob
@@ -13,6 +14,19 @@ from ....utils.json_schema import dict_deep_update
 
 
 def get_metadata(folder_path):
+    """
+    Parse the header of one of the .ncs files to get the session start time (without
+    timezone) and the session_id.
+
+    Parameters
+    ----------
+    folder_path: str
+
+    Returns
+    -------
+    dict
+
+    """
     fpath = glob(folder_path + "/*.ncs")[0]
     with open(fpath, "r", encoding="latin1") as file:
         header = file.read(1024)
@@ -25,7 +39,33 @@ def get_metadata(folder_path):
     return dict(
         session_start_time=session_start_time,
         session_id=session_id,
+        identifier=session_id,
     )
+
+def get_filtering(channel_path):
+    """Get the filtering metadata from an .nsc file.
+
+    Parameters
+    ----------
+    channel_path: str
+        Filepath for an .nsc file
+
+    Returns
+    -------
+    str:
+        json dump of filter parameters. Uses the mu character, which may cause problems
+        for downstream things that expect ASCII.
+    """
+
+    with open(channel_path, "r", encoding="latin1") as file:
+        header = file.read(1024)
+    out = {}
+    for line in text.split("\n\n")[-1].split("\n"):
+        if line[0] == '-':
+            key, val = line.split(' ')
+            out[key[1:]] = val
+
+    return json.dumps(out, ensure_ascii=False)
 
 
 class NeuralynxRecordingInterface(BaseRecordingExtractorInterface):
@@ -36,13 +76,22 @@ class NeuralynxRecordingInterface(BaseRecordingExtractorInterface):
     def __init__(self, folder_path: FolderPathType):
         self.subset_channels = None
         self.source_data = dict(folder_path=folder_path)
-        neuralynx_files = natsorted([str(x) for x in Path(folder_path).iterdir() if ".ncs" in x.suffixes])
-        extractors = [NeuralynxRecordingExtractor(filename=filename, seg_index=0) for filename in neuralynx_files]
+        nsc_files = natsorted([str(x) for x in Path(folder_path).iterdir() if ".ncs" in x.suffixes])
+        extractors = [NeuralynxRecordingExtractor(filename=filename, seg_index=0) for filename in nsc_files]
         gains = [extractor.get_channel_gains()[0] for extractor in extractors]
         for extractor in extractors:
             extractor.clear_channel_gains()
         self.recording_extractor = self.RX(extractors)
         self.recording_extractor.set_channel_gains(gains=gains)
+        try:
+            for i, filename in enumerate(nsc_files):
+                self.recording_extractor.set_channel_property(
+                    i,
+                    "filtering",
+                    get_filtering(filename),
+                )
+        except:
+            warnings.warn("filtering could not be extracted.")
 
     def get_metadata(self):
         return dict_deep_update(
