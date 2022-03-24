25e1db300738b7844819adfab3115f823b6d37b3
bendichter <ben.dichter@gmail.com>
2022-03-24 12:27:46 -0600

    get session_id and session_start_time from neuralynx header

--- nwb_conversion_tools/datainterfaces/ecephys/neuralynx/neuralynxdatainterface.py
+++ nwb_conversion_tools/datainterfaces/ecephys/neuralynx/neuralynxdatainterface.py
@@ -1,11 +1,31 @@
-"""Authors: Cody Baker."""
+"""Authors: Cody Baker and Ben Dichter."""
 from pathlib import Path
 from natsort import natsorted
+from glob import glob
+from dateutil import parser
+
 
 from spikeextractors import MultiRecordingChannelExtractor, NeuralynxRecordingExtractor
 
 from ..baserecordingextractorinterface import BaseRecordingExtractorInterface
 from ....utils import FolderPathType
+from ....utils.json_schema import dict_deep_update
+
+
+def get_metadata(folder_path):
+    fpath = glob(folder_path + "/*.ncs")[0]
+    with open(fpath, "r", encoding="latin1") as file:
+        header = file.read(1024)
+    index = header.find("TimeCreated") + 12
+    session_start_time = parser.parse(header[index:index+19])
+
+    index = header.find("SessionUUID") + 12
+    session_id = header[index:index + 36]
+
+    return dict(
+        session_start_time=session_start_time,
+        session_id=session_id,
+    )
 
 
 class NeuralynxRecordingInterface(BaseRecordingExtractorInterface):
@@ -23,3 +43,10 @@ class NeuralynxRecordingInterface(BaseRecordingExtractorInterface):
             extractor.clear_channel_gains()
         self.recording_extractor = self.RX(extractors)
         self.recording_extractor.set_channel_gains(gains=gains)
+
+    def get_metadata(self):
+        return dict_deep_update(
+            super().get_metadata(),
+            dict(NWBFile=get_metadata(self.source_data["folder_path"]))
+        )
+
