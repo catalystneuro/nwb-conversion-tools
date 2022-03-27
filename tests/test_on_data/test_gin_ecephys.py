5b23ac4e1d74f495dd1021d4471a35b1b0c21aea
Saksham Sharda <saksham20.sharda@gmail.com>
2022-03-27 17:32:21 +0530

    test only behavior

--- tests/test_on_data/test_gin_ecephys.py
+++ tests/test_on_data/test_gin_ecephys.py
@@ -68,7 +68,7 @@ def custom_name_func(testcase_func, param_num, param):
         f"{parameterized.to_safe_name(param.kwargs['data_interface'].__name__)}"
     )
 
-
+@pytest.mark.skip("skip ecephys")
 class TestEcephysNwbConversions(unittest.TestCase):
     savedir = OUTPUT_PATH
 
