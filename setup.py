27d75af88d4dea8e07fe230ebece2221f9e17da1
bendichter <ben.dichter@gmail.com>
2022-04-04 13:04:00 -0400

    refactor path setup to a single place

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ extras_require = dict(full=full_dependencies, test=testing_suite_dependencies)
 
 # Create a local copy for the gin test configuration file based on the master file `base_gin_test_config.json`
 gin_config_file_base = "./base_gin_test_config.json"
-gin_config_file_local = "./tests/test_on_data/gin_test_config.json"
+gin_config_file_local = "gin_test_config.json"
 copy(src=gin_config_file_base, dst=gin_config_file_local)
 
 setup(
