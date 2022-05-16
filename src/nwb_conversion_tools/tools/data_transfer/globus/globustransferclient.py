65fd139891f400af03027cf7a243f2385619384d
Cody Baker <cbaker9@nd.edu>
2022-05-16 16:19:33 -0400

    wip

--- src/nwb_conversion_tools/tools/data_transfer/globus/globustransferclient.py
+++ src/nwb_conversion_tools/tools/data_transfer/globus/globustransferclient.py
@@ -1,14 +1,54 @@
 """Collection of base classes for performing data transfer commands across various services."""
-from .. import BaseTransferClient
+import json
+import os
+from typing import Dict
 
+from .. import BaseTransferClient
 from ..helpers import make_cache
+from ...processes import deploy_process
 from ....utils import FolderPathType, OptionalFolderPathType
+
+
+def _have_globus():
+    try:
+        import globus_cli
+
+        return True
+    except ModuleNotFoundError:
+        return False
+
+
+def _logged_into_globus():
+    # if not os.popen("globus ls 188a6110-96db-11eb-b7a9-f57b2d55370d").read():
+    if not deploy_process("globus ls 188a6110-96db-11eb-b7a9-f57b2d55370d", catch_output=True):
+        return False
 
 
 class GlobusTransferClient(BaseTransferClient):
     """Automated transfers for Globus data."""
 
-    def __init__(self, source_endpoint: str, destination_endpoint: str, cache_folder: OptionalFolderPathType = None):
+    installation_message = "Please install the Globus CLI to use the GlobusTransferClient! (`pip install globus-cli`)"
+    logged_in_message = (
+        "You must login to the Globus service through the CLI! (`globus login`, then follow instructions)"
+    )
+
+    def __init__(self, source_endpoint_id: str, destination_endpoint: str, cache_folder: OptionalFolderPathType = None):
+        """
+        Automatically manage Globus task submissions.
+
+        Parameters
+        ----------
+        source_endpoint_id : str
+          DESCRIPTION.
+        destination_endpoint : str
+          DESCRIPTION.
+        cache_folder : OptionalFolderPathType, optional
+          DESCRIPTION. The default is None.
+        """
+        assert _have_globus(), self.installation_message
+        assert _logged_into_globus(), self.logged_in_message
+
+        self.source_endpoint_id = source_endpoint_id
         self.cache_folder = make_cache(cache_folder=cache_folder)
         # Some child kwargs may involve candidates for optional keyword argument `parent_directory` in `make_cache`
         # So in those cases do not call super().__init__(...) and remember to record the kwargs
@@ -16,12 +56,12 @@ class GlobusTransferClient(BaseTransferClient):
     def fetch_total_manifest(
         self, source_directory: FolderPathType, recursive: bool = True, timeout: float = 120.0
     ) -> Dict[str, int]:
-        assert HAVE_GLOBUS, "You must install the globus CLI (pip install globus-cli)!"
+        assert _logged_into_globus(), self.logged_in_message
 
         recursive_flag = " --recursive" if recursive else ""
         contents = json.loads(
             deploy_process(
-                command=f"globus ls -Fjson {globus_endpoint_id}:{path}{recursive_flag}",
+                command=f"globus ls -Fjson {self.source_endpoint_id}:{source_directory}{recursive_flag}",
                 catch_output=True,
                 timeout=timeout,
             )
@@ -30,4 +70,6 @@ class GlobusTransferClient(BaseTransferClient):
         return files_and_sizes
 
     def transfer_data(self):
-        raise NotImplementedError(f"The method 'transfer_data' has not been defined for class {self.__class__}!")
+        assert _logged_into_globus(), self.logged_in_message
+
+        pass
