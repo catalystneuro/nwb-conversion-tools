926bf702d6ed2d81faa4a04e6f914661d4385929
bendichter <ben.dichter@gmail.com>
2022-05-25 18:15:42 -0400

    fix a few minor bugs

--- src/nwb_conversion_tools/datainterfaces/ophys/scanimage/scanimageimaginginterface.py
+++ src/nwb_conversion_tools/datainterfaces/ophys/scanimage/scanimageimaginginterface.py
@@ -1,4 +1,6 @@
 import json
+from dateutil.parser import parse as dateparse
+from typing import Optional
 
 try:
     from PIL import Image, ExifTags
@@ -8,47 +10,49 @@ except ImportError:
     HAVE_PIL = False
 
 from ..tiff.tiffdatainterface import TiffImagingInterface
-from ....utils import dict_deep_update
+from ....utils import dict_deep_update, FilePathType, OptionalArrayType
+
 
 class ScanImageImagingInterface(TiffImagingInterface):
+    def __init__(
+        self,
+        file_path: FilePathType,
+        fallback_sampling_frequency: Optional[float] = None,
+        channel_names: OptionalArrayType = None,
+    ):
+        """
 
-    def __init__(self, file_path: FilePathType, channel_names: ArrayType = None):
+        Parameters
+        ----------
+        file_path: str
+        fallback_sampling_frequency: float, optional
+            The sampling frequency can usually be extracted from the scanimage metadata in
+            exif:ImageDescription:state.acq.frameRate. If not, use this.
+        channel_names: list
+            list of channel names
+        """
 
-        assert (
-            HAVE_PIL
-        ), "To use the ScanImageTiffExtractor install Pillow: \n\n pip install pillow\n\n"
-        image = Image.open(filename)
+        assert HAVE_PIL, "To use the ScanImageTiffExtractor install Pillow: \n\n pip install pillow\n\n"
+        image = Image.open(file_path)
         image_exif = image.getexif()
-        exif = {
-            ExifTags.TAGS[k]: v
-            for k, v in image_exif.items()
-            if k in ExifTags.TAGS and type(v) is not bytes
-        }
+        exif = {ExifTags.TAGS[k]: v for k, v in image_exif.items() if k in ExifTags.TAGS and type(v) is not bytes}
         self.image_metadata = {
-            x.split("=")[0]: x.split("=")[1]
-            for x in exif["ImageDescription"].split("\r")
-            if "=" in x
+            x.split("=")[0]: x.split("=")[1] for x in exif["ImageDescription"].split("\r") if "=" in x
         }
-
-        sampling_frequency = float(self.image_description["state.acq.frameRate"])
+        if "state.acq.frameRate" in self.image_metadata:
+            sampling_frequency = float(self.image_metadata["state.acq.frameRate"])
+        else:
+            sampling_frequency = fallback_sampling_frequency
 
         super().__init__(file_path=file_path, sampling_frequency=sampling_frequency, channel_names=channel_names)
 
     def get_metadata(self):
-        session_start_time = parser.parse(
-            self.image_metadata["state.internal.triggerTimeString"]
-        )
 
-        metadata = super().get_metadata()
-        new_metadata = dict(
-            NWBFile=dict(
-                session_start_time=session_start_time),
-            Ophys=dict(
-                TwoPhotonSeries=dict(
-                    description=json.dumps(self.image_metadata)
-                )
+        new_metadata = dict(Ophys=dict(TwoPhotonSeries=dict(description=json.dumps(self.image_metadata))))
+
+        if "state.internal.triggerTimeString" in self.image_metadata:
+            new_metadata["NWBFile"] = dict(
+                session_start_time=dateparse(self.image_metadata["state.internal.triggerTimeString"])
             )
-        )
-        metadata = dict_deep_update(metadata, new_metadata)
 
-        return metadata
+        return dict_deep_update(super().get_metadata(), new_metadata)
