0b2831bc7cfc648e392da35dd7ce1654ca325c07
weiglszonja <wszonja@gmail.com>
2022-06-30 13:56:47 +0200

    add first unittest

--- tests/test_internals/test_tools/test_hdmf.py
+++ tests/test_internals/test_tools/test_hdmf.py
@@ -1,7 +1,9 @@
 import numpy as np
 from numpy.testing import assert_array_equal
 
-from nwb_conversion_tools.tools.hdmf import SliceableDataChunkIterator
+from nwb_conversion_tools.tools.hdmf import SliceableDataChunkIterator, ImagingExtractorDataChunkIterator
+
+from roiextractors.testing import generate_dummy_imaging_extractor
 
 
 def test_sliceable_data_chunk_iterator():
@@ -18,3 +20,20 @@ def test_sliceable_data_chunk_iterator():
         data_chunk.data,
         [[0, 1, 2, 3, 4], [10, 11, 12, 13, 14], [20, 21, 22, 23, 24], [30, 31, 32, 33, 34], [40, 41, 42, 43, 44]],
     )
+
+
+def test_roi_extractors_data_chunk_iterator():
+    roi_extractor = generate_dummy_imaging_extractor(num_frames=100)
+
+    dci = ImagingExtractorDataChunkIterator(
+        imaging_extractor=roi_extractor,
+        buffer_gb=0.00005,
+        chunk_mb=0.001,
+    )
+
+    data_chunk = next(dci)
+
+    assert data_chunk.selection == (slice(0, 100, None), slice(0, 10, None), slice(0, 10, None))
+
+    assert_array_equal(data_chunk.data, roi_extractor.get_video(start_frame=0,
+                                                                end_frame=100).transpose((0, 2, 1)))
