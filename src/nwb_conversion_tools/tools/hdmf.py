f7182fa3542fe057ca3ac822dbea8d1d057eb4a8
weiglszonja <wszonja@gmail.com>
2022-07-04 13:14:54 +0200

    warn if the first axis of buffer_shape is not a multiple of chunk_shape and override chunk_shape to the closest multiple

--- src/nwb_conversion_tools/tools/hdmf.py
+++ src/nwb_conversion_tools/tools/hdmf.py
@@ -1,5 +1,6 @@
 """Collection of modifications of HDMF functions that are to be tested/used on this repo until propagation upstream."""
 from typing import Tuple, Optional
+from warnings import warn
 
 import numpy as np
 from hdmf.data_utils import GenericDataChunkIterator as HDMFGenericDataChunkIterator
@@ -130,9 +131,16 @@ class ImagingExtractorDataChunkIterator(GenericDataChunkIterator):
                 f"must be equal to max shape ({self._maxshape})."
             )
 
+        if buffer_shape[0] % chunk_shape[0] != 0:
+            # warn if the first axis is not a multiple of the chunk shape
+            warn(
+                f"The first axis of the buffer shape ({buffer_shape[0]}) must be divisible by the first axis of the chunk shape ({chunk_shape[0]})"
+            )
+            # override the chunk shape?
+            chunk_shape = tuple([chunk_shape[0] - (buffer_shape[0] % chunk_shape[0])]) + chunk_shape[1:]
+
         super().__init__(
             buffer_shape=buffer_shape,
-            chunk_mb=chunk_mb,
             chunk_shape=chunk_shape,
             display_progress=display_progress,
             progress_bar_options=progress_bar_options,
